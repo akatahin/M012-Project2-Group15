96a162dc9955b9482ec3148610dafbe452a483bf
abdurcheema <91966788+abdurcheema@users.noreply.github.com>
2021-11-10 23:30:17 -0500

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -86,86 +86,56 @@ def exploit():
 
 exploit()
 
-def eGreedy(e: int):
-    pass
-
-# • You will visit one of the cafeterias each day for 300 days.
-# • There are three cafeterias: C1, C2, and C3
-# • The cafeterias have happiness values that are normally distributed
-# with the following mean and standard deviation values:
-# o C1: average happiness = 9, standard deviation = 3
-# o C2: average happiness = 7, standard deviation = 5
-# o C3: average happiness = 11, standard deviation = 7
-
-# Returns the total happiness where the first three days
-# each cafeteria is used.
 import random
 def eGreedy(e: int) -> int:
     totalHappy = 0
-    percent = e / 100  # e as a percentage
-    print(percent)
-    day1Happiness = random.normalvariate(9, 3)  # first visit to caf 1
-    day2Happiness = random.normalvariate(7, 5)  # first visit to caf 2
-    day3Happiness = random.normalvariate(11, 7)  # first visit to caf 3
+    percent = e // 100 # e as a percentage
+    print(percent, "d")
+    print(100 * random.random())
+    day1Happiness = random.normalvariate(9, 3)  # first visit to cafeteria 1
+    day2Happiness = random.normalvariate(7, 5)  # first visit to cafeteria 2
+    day3Happiness = random.normalvariate(11, 7)  # first visit to cafeteria 3
 
-# hap(1-3) is the variable that tracks the total hap for respective caf
-    hap1 = day1Happiness
-    hap2 = day2Happiness
-    hap3 = day3Happiness
-    print(hap1, hap2, hap3)
-
-# next three if/elif/else statments establish the best caf after first three days
-    if day1Happiness > day2Happiness and day1Happiness > day3Happiness:
-        bestCafeteria = "Cafeteria 1"
-        best = day1Happiness
-    elif day2Happiness > day1Happiness and day2Happiness > day3Happiness:
-        bestCafeteria = "Cafeteria 2"
-        best = day2Happiness
-    else:
-        bestCafeteria = "Cafeteria 3"
-        best = day3Happiness
-    print(bestCafeteria, totalHappy)
-    totalHappy += best
-    print(bestCafeteria, totalHappy)
+    # happy(1-3) is the variable that tracks the total happiness for respective cafeteria
+    happy1 = day1Happiness
+    happy2 = day2Happiness
+    happy3 = day3Happiness
+    print(happy1, happy2, happy3)
+    day1, day2, day3 = 1, 1, 1
 
     for i in range(297):
-        r = random.random() # generates the random number between 0-1
-        print(r)
-        if r < percent:  # this if statement is for when you are picking a random caf
-            caf = random.randint(1, 3)
-            if caf == 1:
-                c1Hap = random.normalvariate(9, 3)
-                totalHappy = totalHappy + c1Hap
-                hap1 = hap1 + c1Hap
-            elif caf == 2:
-                c2Hap = random.normalvariate(7, 5)
-                totalHappy = totalHappy + c2Hap
-                hap2 = hap2 + c2Hap
-            elif caf == 3:
-                c3Hap = random.normalvariate(11, 7)
-                totalHappy = totalHappy + c3Hap
-                hap3 = hap3 + c3Hap
-        else:  # this else is running the best caf when r > percent (e%)
-            if bestCaf == "c1":
-                c1Hap = random.normalvariate(9, 3)
-                totalHappy = totalHappy + c1Hap
-                hap1 += c1Hap
-            elif bestCaf == "c2":
-                c2Hap = random.normalvariate(7, 5)
-                totalHappy = totalHappy + c2Hap
-                hap2 += c2Hap
-            elif bestCaf == "c3":
-                c3Hap = random.normalvariate(11, 7)
-                totalHappy = totalHappy + c3Hap
-                hap3 += c3Hap
-        if hap1 > hap2 and hap1 > hap3:  # next three if statements resets what the best caf is after each run of the for loop
-            bestCaf = "c1"
-        elif hap2 > hap1 and hap2 > hap3:
-            bestCaf = "c2"
-        elif hap3 > hap2 and hap3 > hap1:
-            bestCaf = "c3"
-    return totalHappy
+        randomValue = random.random()
+        if randomValue < percent:
+            cafeteria = random.randint(1,3)
+            print(randomValue)
+        else:
+            if day1Happiness > day2Happiness and day1Happiness > day3Happiness:
+                bestCafeteria = "Cafeteria 1"
+                cafeteria = 1
+            elif day2Happiness > day1Happiness and day2Happiness > day3Happiness:
+                bestCafeteria = "Cafeteria 2"
+                cafeteria = 2
+            else:
+                bestCafeteria = "Cafeteria 3"
+                cafeteria = 3
 
+        if cafeteria == 1:
+            happy1 += random.normalvariate(9, 3)
+            day1 += 1
 
-print(eGreedy(12))  # runs eGreedy with 12 percent greedy
+        elif cafeteria == 2:
+            happy2 += random.normalvariate(7, 5)
+            day2 += 1
+
+        else:
+            happy3 += random.normalvariate(11, 7)
+            day3 += 1
+
+        day1Happiness = happy1 / day1
+        day2Happiness = happy2 / day2
+        day3Happiness = happy3 / day3
+    total = happy1 + happy2 + happy3
+    return total
+
+print(eGreedy(12))
 
