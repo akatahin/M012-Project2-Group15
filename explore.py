92678cf27902f944aa3d40d30bcbe0b1258ee59e
Raasin Amin Rahman <77879314+RaasinRahman@users.noreply.github.com>
2021-11-10 20:10:16 -0500

    Update explore.py

--- explore.py
+++ explore.py
@@ -1,21 +1,20 @@
-
 import random
 
 def explore():
     count = 0
-    for i in range(100):
-        C1 = 9
-        SD = 3
-        r1 = (random.normalvariate(C1, SD))
-        count = count + r1
+    for i in range(100): #100 trials
+        C1 = 9 #cafeteria 1 AVG Hapiness
+        SD = 3 #cafeteria 1 St. Dev
+        r1 = (random.normalvariate(C1, SD)) #random number generator based on AVG and st.Dev
+        count = count + r1 #adding each random number to the total
     print("Cafeteria 1 Hapiness: ")
     print(count)
 
     count2 = 0
     for i in range(100):
-        C2 = 7
-        SD2 = 5
-        r2 = (random.normalvariate(C2, SD2))
+        C2 = 7 #cafeteria 2 AVG Hapiness
+        SD2 = 5 #cafeteria 2 St. Dev
+        r2 = (random.normalvariate(C2, SD2)) #random number generator based on AVG and st.Dev
         count2 = count + r2
     print("Cafeteria 2 Hapiness: ")
     print(count2)
@@ -23,9 +22,9 @@ def explore():
     count3 = 0
 
     for i in range(100):
-        C3 = 11
-        SD3 = 7
-        r3 = (random.normalvariate(C3, SD3))
+        C3 = 11 #cafeteria 3 St. Dev
+        SD3 = 7 #cafeteria 3 St. Dev
+        r3 = (random.normalvariate(C3, SD3)) #random number generator based on AVG and st.Dev
         count3 = count + r3
     print("Cafeteria 3 Hapiness: ")
     print(count3)
@@ -33,6 +32,9 @@ def explore():
     sum = count + count2 + count3
     print("Sum: ")
     print(sum)
-    return sum 
+    return sum
+
+
+
 
 explore()
