b4f7cfa14675f8b70f379e752e0c239f489ab8c9
Raasin Amin Rahman <77879314+RaasinRahman@users.noreply.github.com>
2021-11-11 03:00:45 -0500

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -1,90 +1,79 @@
 import random
 
+
 def explore():
     count = 0
-    for i in range(100): #100 trials
-        C1 = 9 #cafeteria 1 AVG Hapiness
-        SD = 3 #cafeteria 1 St. Dev
-        r1 = (random.normalvariate(C1, SD)) #random number generator based on AVG and st.Dev
-        count = count + r1 #adding each random number to the total
-    print("Cafeteria 1 Hapiness: ")
-    print(count)
+    for i in range(100):  # 100 trials
+        C1 = 9  # cafeteria 1 AVG Hapiness
+        SD = 3  # cafeteria 1 St. Dev
+        r1 = (random.normalvariate(C1, SD))  # random number generator based on AVG and st.Dev
+        count = count + r1  # adding each random number to the total
 
     count2 = 0
     for i in range(100):
-        C2 = 7 #cafeteria 2 AVG Hapiness
-        SD2 = 5 #cafeteria 2 St. Dev
-        r2 = (random.normalvariate(C2, SD2)) #random number generator based on AVG and st.Dev
+        C2 = 7  # cafeteria 2 AVG Hapiness
+        SD2 = 5  # cafeteria 2 St. Dev
+        r2 = (random.normalvariate(C2, SD2))  # random number generator based on AVG and st.Dev
         count2 = count + r2
-    print("Cafeteria 2 Hapiness: ")
-    print(count2)
 
     count3 = 0
 
     for i in range(100):
-        C3 = 11 #cafeteria 3 AVG Hapiness
-        SD3 = 7 #cafeteria 3 St. Dev
-        r3 = (random.normalvariate(C3, SD3)) #random number generator based on AVG and st.Dev
+        C3 = 11  # cafeteria 3 AVG Hapiness
+        SD3 = 7  # cafeteria 3 St. Dev
+        r3 = (random.normalvariate(C3, SD3))  # random number generator based on AVG and st.Dev
         count3 = count + r3
-    print("Cafeteria 3 Hapiness: ")
-    print(count3)
-
-    sum = count + count2 + count3 #adding all of the total hapiness from 3 cafertias
-    print("Sum: ")
-    print(sum)
-    return sum #returns total hapiness
 
 
+    sum = count + count2 + count3  # adding all of the total hapiness from 3 cafertias
+    #print("Sum Explore: ")
+    #print(sum)
+    return sum  # returns total hapiness
 
 
-explore()
 
 
 def exploit():
-    print("----------------------------")
+    #print("----------------------------")
     total = 0
 
     C1 = 9
     SD = 3
     r1 = (random.normalvariate(C1, SD))
     total = total + r1
-    print(r1)
+
 
     C2 = 7
     SD2 = 5
     r2 = (random.normalvariate(C2, SD2))
     total = total + r2
-    print(r2)
+
 
     C3 = 11
     SD3 = 7
     r3 = (random.normalvariate(C3, SD3))
     total = total + r3
-                                                  
-    r = max(r1, r2, r3)
-    print(r3)
-    print(r)
 
-    if(r == r1):
+    r = max(r1, r2, r3)
+
+    if (r == r1):
         A = 9
         SD = 3
 
-    if(r == r2):
+    if (r == r2):
         A = 7
         SD = 5
-    if(r == r3):
+    if (r == r3):
         A = 11
         SD = 7
-    
+
     for i in range(297):
         p = (random.normalvariate(A, SD))
         total = total + p
-    print("Sum: ")
-    print(total)
+    #print("Sum Exploit: ")
+    #print(total)
+    return total
 
-
-
-exploit()
 
 
 def eGreedy(e: int) -> int:
@@ -98,7 +87,7 @@ def eGreedy(e: int) -> int:
     happy2 = day2Happiness
     happy3 = day3Happiness
 
-    print("Starting Happiness Rating of Cafe (1-3):\t", happy1, happy2, happy3)
+    #print("Starting Happiness Rating of Cafe (1-3):\t", happy1, happy2, happy3)
     day1, day2, day3 = 1, 1, 1
 
     # ignore h, v - just to check
@@ -149,13 +138,73 @@ def eGreedy(e: int) -> int:
         day2Happiness = happy2 / day2
         day3Happiness = happy3 / day3
 
-    print("Picked cafe:", h, "  ||  ", "Random cafe:", v)
-    print("Average Happiness Rating of Cafe (1-3): \t", day1Happiness, day2Happiness, day3Happiness)
+    #print("Picked cafe:", h, "  ||  ", "Random cafe:", v)
+    #print("Average Happiness Rating of Cafe (1-3): \t", day1Happiness, day2Happiness, day3Happiness)
 
     totalHappy = happy1 + happy2 + happy3
-    print("\n\nTotal Happiness of 300 days:", totalHappy)
+    #print("\n\nSum E-Greedy:", totalHappy)
 
     return totalHappy
 
 
-eGreedy(12)
+
+def simulation(e: int, t: int):
+
+    exploitResult = 0
+    exploreResult = 0
+    greedyResult = 0
+
+    for i in range(t):
+        exploitResult += exploit()
+        exploreResult += explore()
+        greedyResult += eGreedy(e)
+    exploreResult /= t
+    exploitResult /= t
+    greedyResult /= t
+
+    Averages = {"C1": 9, "C2": 7, "C3": 11}
+
+    Values = Averages.values()
+
+
+    Keys = Averages.keys()
+
+    maxVal = max(Values)
+
+    optimumHappiness = maxVal * 300
+
+    expectedHappiness = (Averages['C1'] * 100 + Averages['C2'] * 100 + Averages['C3'] * 100)
+
+    expectedHappiness2 = (Averages['C1'] + Averages['C2'] + Averages['C3']) + (297 * maxVal)
+
+    expectedHappiness3 = (Averages['C1'] * 264 + Averages['C1'] * 12 + Averages['C2'] * 12 + Averages['C2'] * 12)
+
+
+
+    print("Explore Method Trials:")
+    print("Optimum Hapiness: " + str(optimumHappiness))
+    print("Expected Hapiness: " + str(expectedHappiness))
+    print("Average Hapiness " + str(exploreResult))
+    print("Expected Regret: " + str(optimumHappiness - expectedHappiness))
+    print("Simulated Regret: " + str(optimumHappiness - exploreResult))
+
+    print("__________________________________")
+
+    print("Exploit Method Trials:")
+    print("Optimum Hapiness: " + str(optimumHappiness))
+    print("Expected Hapiness: " + str(expectedHappiness2))
+    print("Average Hapiness " + str(exploitResult))
+    print("Expected Regret: " + str(optimumHappiness - expectedHappiness2))
+    print("Simulated Regret: " + str(optimumHappiness - exploitResult))
+
+    print("__________________________________")
+
+    print("eGreedy Method Trials:")
+    print("Optimum Hapiness: " + str(optimumHappiness))
+    print("Expected Hapiness: " + str(expectedHappiness3))
+    print("Average Hapiness " + str(greedyResult))
+    print("Expected Regret: " + str(optimumHappiness - expectedHappiness3))
+    print("Simulated Regret: " + str(optimumHappiness - greedyResult))
+
+
+simulation(12, 10)
